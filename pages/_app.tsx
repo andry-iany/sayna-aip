837229b1053417d0c2992895cd2bb7c31968ca96
Andry <asitrakiniainasoa@gmail.com>
2022-10-13 14:13:46 +0300

    add deps for i18n

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import "leaflet/dist/leaflet.css";
 
 import type { AppProps } from "next/app";
 
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { SSRProvider } from "react-bootstrap";
 import Layout from "../components/Layout";
 
@@ -12,22 +12,33 @@ import Layout from "../components/Layout";
 import "../setup/progress/style.css";
 import { setupNavProgress } from "../setup/progress";
 
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+import en from "../lang/en.json";
+import fr from "../lang/fr.json";
+
+const allMessages = { en, fr };
+type Locale = keyof typeof allMessages;
+
 setupNavProgress();
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { locale = "fr" } = useRouter();
+  const messages = useMemo(() => allMessages[locale as Locale], [locale]);
+
   useEffect(() => {
     // @ts-ignore
     import("bootstrap/dist/js/bootstrap.bundle.js");
   }, []);
 
   return (
-    <>
+    <IntlProvider locale={locale} messages={messages}>
       <SSRProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </SSRProvider>
-    </>
+    </IntlProvider>
   );
 }
 
