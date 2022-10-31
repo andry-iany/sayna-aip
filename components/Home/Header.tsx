cf06c041a153f4c823ccc723eb7489a1d82075c4
Andry <asitrakiniainasoa@gmail.com>
2022-10-31 14:03:47 +0300

    add typing effect

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,22 +1,48 @@
 import classes from "./style.module.css";
 import Button from "../ui/Button";
 import Mark from "../ui/Mark";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
+const questions = [
+  "Vous avez une idée ?",
+  "Vous avez un projet?",
+  "Vous avez déjà vos premiers clients ?",
+];
 
 const Header = () => {
+  const questionEltRef = useRef<HTMLSpanElement | null>(null);
+
+  useEffect(() => {
+    const headingElt = questionEltRef.current;
+    if (!headingElt) return;
+
+    const typed = new Typed(headingElt, {
+      strings: questions,
+      typeSpeed: 40,
+      backSpeed: 50,
+      loop: true,
+      showCursor: true,
+      cursorChar: "|",
+      backDelay: 3000,
+      startDelay: 700,
+    });
+
+    return () => typed.destroy();
+  }, []);
+
   return (
     <header
-      className={classes.header + " min-vh-100 d-flex align-items-center "}
+      className={classes.header + " min-vh-100 d-flex align-items-center"}
     >
-      <section className={classes.pageTitle + " c-text-secondary"}>
+      <section className={classes.pageTitle}>
         <h2 className="h5 mb-4 fw-normal">
-          Vous avez une idée ? Un projet ?
-          <br />
-          Vous avez déjà vos premiers clients ?
+          <span ref={questionEltRef}></span>
         </h2>
 
-        <h1 className="text-uppercase mb-5">
+        <h1 className="mb-5">
           Faites-vous accompagner par nos <Mark>experts</Mark> et garantissez
-          votre <Mark>réussite</Mark>!
+          votre <Mark>réussite</Mark>
         </h1>
 
         <Button img="/images/arrow-right.svg" href="/solutions">
