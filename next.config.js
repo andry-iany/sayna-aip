70d5a8075065970acf9572635c45db83a5bab9a1
Andry <asitrakiniainasoa@gmail.com>
2022-11-03 17:02:10 +0300

    remove logging from next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const { i18n } = require("./next-i18next.config");
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-const url = new URL(baseUrl);
-
-console.log({ url });
 
 const nextConfig = {
   reactStrictMode: true,
