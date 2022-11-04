e2000c4d0f6e38cba33abdba9665f11e4b4ba883
Andry <asitrakiniainasoa@gmail.com>
2022-11-04 16:17:19 +0300

    add translation to contact page

--- components/Contact/index.tsx
+++ components/Contact/index.tsx
@@ -1,60 +1,41 @@
 import Image from "next/image";
+import { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+import { ContentContext } from "../../pages/contact";
 import AnimateOnView from "../ui/AnimateOnView";
 import PageSectionTitle from "../ui/PageSectionTitle";
 import ContactForm from "./ContactForm";
 import classes from "./style.module.css";
 
-const contacts = [
-  {
-    country: "Togo",
-    email: "freddkoudahoua@gmail.com",
-    phone: "+228798609",
-    location:
-      "Nukafu, Rue en face de la Direction Générale de la SOTRAL, Immeuble MENSANH",
-  },
-  {
-    country: "Côte d'Ivoire",
-    phone: "+2250101460000",
-    location: "Cocody Ambassades, A57 Rue du Bélier, Villa COLOMB",
-  },
-  {
-    country: "Benin",
-    location: "Cité vie nouvelle",
-  },
-  {
-    country: "Niger",
-    location: "Niamey",
-  },
-];
-
 const Contact = () => {
-  const renderContacts = contacts.map((contact) => (
-    <Col key={contact.country} className={classes.country + " px-3 mb-5"}>
+  const content: any = useContext(ContentContext);
+
+  const renderContacts = content.contact?.map((contact: any) => (
+    <Col key={contact?.id} className={classes.country + " px-3 mb-5"}>
       <div className="border p-3 h-100 shadow-sm">
-        <h3 className="mb-4">{contact.country} </h3>
+        <h3 className="mb-4">{contact?.country} </h3>
         <div className={"d-flex align-items-start gap-3"}>
           <span className={classes.img}>
             <Image src="/images/location.svg" layout="fill" alt="" />
           </span>
-          <p className="mb-3">{contact.location}</p>
+          <p className="mb-3">{contact?.address}</p>
         </div>
 
-        {contact.phone && (
+        {contact?.phone && (
           <div className={"d-flex align-items-start gap-3"}>
             <span className={classes.img}>
               <Image src="/images/phone.svg" layout="fill" alt="" />
             </span>
-            <p className="mb-3">{contact.phone}</p>
+            <p className="mb-3">{contact?.phone}</p>
           </div>
         )}
 
-        {contact.email && (
+        {contact?.email && (
           <div className={"d-flex align-items-start gap-3"}>
             <span className={classes.img}>
               <Image src="/images/email.svg" layout="fill" alt="" />
             </span>
-            <p className="mb-3">{contact.email}</p>
+            <p className="mb-3">{contact?.email}</p>
           </div>
         )}
       </div>
@@ -64,7 +45,10 @@ const Contact = () => {
   return (
     <Container className={classes.container}>
       <AnimateOnView>
-        <PageSectionTitle title="Nos Contacts" subtitle="" />
+        <PageSectionTitle
+          title={content?.page_section_heading_1?.title}
+          subtitle={content?.page_section_heading_1?.subtitle}
+        />
       </AnimateOnView>
       <AnimateOnView amount={0.2}>
         <Row xs={1} md={2} className={classes.contactDetail + " mx-auto mb-5"}>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,10 +1,14 @@
 import type { GetStaticProps, NextPage } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
+import { createContext } from "react";
 import ContactComponent from "../components/Contact";
 import { useTranslation } from "../hooks";
+import api from "../services/apiService";
 
-const Contact: NextPage = () => {
+export const ContentContext = createContext({});
+
+const Contact: NextPage = (props: any) => {
   const t = useTranslation();
 
   return (
@@ -14,7 +18,9 @@ const Contact: NextPage = () => {
         <meta name="description" content={t("meta.description_contact")} />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ContactComponent />
+      <ContentContext.Provider value={props["data"]}>
+        <ContactComponent />
+      </ContentContext.Provider>
     </div>
   );
 };
@@ -24,9 +30,16 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
   const localizedData = await serverSideTranslations(locale, ["common"]);
 
+  const contactContentRes = await api.get<any>(
+    `/contact?populate=deep&locale=${locale}`
+  );
+
+  const contactContent = contactContentRes.data?.data?.attributes;
+
   return {
     props: {
       ...localizedData,
+      data: contactContent,
     },
   };
 };
